e3176249206394a77aea14cc60112f762b04f32e
shafi3716 <abdullahalshafi1212@gmail.com>
2023-08-20 16:10:24 +0600

    env-cmd package added.

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>This is The Nordcom Ecommerce Public Panel</h1>
+      <p>API base url get from env '{process.env.API_BASE_URL}'</p>
     </main>
   );
 }
