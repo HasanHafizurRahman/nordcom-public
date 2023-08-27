7105558b46e5d0b69de778af2d1d42802de54042
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-08-27 13:23:24 +0600

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,23 +1,10 @@
 "use client";
-import Nav2 from "./Header/Nav2";
-import TopNav from "./Header/TopNav";
-import Nav1 from "./Header/Nav1";
-import MobileNavbar from "./Header/MobileNavbar";
+
 import Banner from "./Banner/Banner";
 
 const HomePage = () => {
   return (
     <div>
-      {/* Mobile View  */}
-      <div className="lg:hidden">
-        <MobileNavbar />
-      </div>
-      {/* Desktop View  */}
-      <div className="hidden lg:block">
-        <TopNav />
-        <Nav1 />
-        <Nav2 />
-      </div>
       <Banner />
     </div>
   );
