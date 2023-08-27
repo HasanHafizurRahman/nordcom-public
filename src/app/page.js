001fa46cae266682eec37e737746180fe0e6947f
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-08-27 13:22:45 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
+import MobileNavbar from "@/components/Header/MobileNavbar";
+import Nav1 from "@/components/Header/Nav1";
+import Nav2 from "@/components/Header/Nav2";
+import TopNav from "@/components/Header/TopNav";
 import HomePage from "@/components/HomePage";
 
 export default function Home() {
   return (
     <main>
+      {/* Mobile View  */}
+      <div className="lg:hidden">
+        <MobileNavbar />
+      </div>
+      {/* Desktop View  */}
+      <div className="hidden lg:block">
+        <TopNav />
+        <Nav1 />
+        <Nav2 />
+      </div>
       <HomePage />
       <h1>This is The Nordcom Ecommerce Public Panel</h1>
       <p>API base url get from env '{process.env.API_BASE_URL}'</p>
